1825208228def783d3ba62d76c344dfc327a8ea9
EnzoFEUR <lorenzomiguelnarte@gmail.com>
2026-08-19 09:07:59 +0800

    add comments

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,5 +1,5 @@
 // C-Point HRIS Progressive Web App Service Worker (Network-First Navigation Strategy)
-const CACHE_NAME = 'cpoint-hris-v2.2.4';
+const CACHE_NAME = 'cpoint-hris-v2.2.5';
 const STATIC_ASSETS = [
   '/',
   '/index.html',
